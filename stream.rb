b47c299d4288fa4e863980cd2913b388243d2493
Daniel Hollands <daniel@limeblast.co.uk>
2016-02-27 18:25:46 +0000

    Script now puts tweets in which BabblePi is mentioned

--- stream.rb
+++ stream.rb
@@ -1,8 +1,8 @@
 require 'rubygems'
 require 'bundler/setup'
-
 require 'dotenv'
 require 'twitter'
+
 Dotenv.load
 
 client = Twitter::Streaming::Client.new do |config|
@@ -12,17 +12,16 @@ client = Twitter::Streaming::Client.new do |config|
   config.access_token_secret = ENV["ACCESS_SECRET"]
 end
 
-# San Fran
-#client.filter(locations: "-122.75,36.8,-121.75,37.8") do |tweet|
-#  puts tweet.text
-#end
-
 client.user do |object|
   case object
   when Twitter::Tweet
-    puts object.in_reply_to_screen_name
-    puts object.in_reply_to_screen_name?
-    puts object.user_mentions
-    puts object.user_mentions?
+    tweet = object
+    if tweet.user_mentions?
+      tweet.user_mentions.each do |mention|
+        if mention.screen_name == 'BabblePi'
+          puts tweet.text
+        end
+      end
+    end
   end
 end
